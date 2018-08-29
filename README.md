33d1348de397690d56a4c0d15be35ea1b349647d
Alex Hoyau <a.hoyau@silexlabs.org>
2018-08-29 17:16:49 +0200

    docs

--- README.md
+++ README.md
@@ -12,11 +12,22 @@ Use `npm install node-red-contrib-hangouts-chat` to install.
 
 This package provides nodes to send and receive messages on [Hangouts Chat](https://chat.google.com/) via Node-RED. 
 
-You will need a google service account to use the send node.
-
 The input node is used to receive messages from hangouts chat. You can use `msg.roomId` to filter incoming messages by conversation. The message contains the message in msg.payload, the conversation id in msg.roomId and the complete message event object in msg.event.
 
 The output node is used to send messages to an existing conversation. You can use the conversation id on the properties or provide a valid conversation id in `msg.roomId`. The `msg.payload` will be send to the conversation.
+
+You will need to create a project in Google cloud platform with Hangouts chat enabled. To do so, login to [developer console](https://console.developers.google.com/). Create a new project, and enable Hangout Chat API. Under configuration, set:
+
+* status: live
+* bot name (this is how you will add and ping the bot)
+* avatar
+* description
+* functionality: rooms
+* connection settings – bot URL: <your node-red server HTTPS url>
+* permission: everyone in your domain
+
+
+You will need a google service account to use the send node.
 
 ## License
 
--- hangouts-chat.js
+++ hangouts-chat.js
@@ -1,7 +1,6 @@
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const {google} = require('googleapis');
-const fs = require('fs');
 const unirest = require('unirest');
 
 module.exports = function(RED) {
@@ -9,20 +8,20 @@ module.exports = function(RED) {
   function HangoutsChatInNode(config) {
     RED.nodes.createNode(this, config);
     var node = this;
-      
-			node.status({fill:"grey",shape:"dot",text:"waiting for message"});
-      RED.httpNode.post('/hangouts-chat/bot-url', bodyParser.json(), bodyParser.urlencoded({extended:true}),onPost);
-      RED.httpNode.get('/hangouts-chat/test', (req, res) => res.sendStatus(200));
+
+    node.status({fill:"grey",shape:"dot",text:"waiting for message"});
+    RED.httpNode.post('/hangouts-chat/bot-url', bodyParser.json(), bodyParser.urlencoded({extended:true}),onPost);
+    RED.httpNode.get('/hangouts-chat/test', (req, res) => res.sendStatus(200));
     function onPost(req,res) {
-			node.status({fill:"green",shape:"dot",text:"connected"});
+      node.status({fill:"green",shape:"dot",text:"connected"});
       const roomId = req.body.space.name;
       console.log('someone pinged @<bot-name>', roomId);
 
       node.send({
-				payload: req.body.message.text,
-				roomId: roomId,
-				event: req.body,
-			});
+        payload: req.body.message.text,
+        roomId: roomId,
+        event: req.body,
+      });
 
       return res.json({
       });
@@ -34,84 +33,100 @@ module.exports = function(RED) {
 
   function HangoutsChatOutNode(config) {
     RED.nodes.createNode(this, config);
-    var node = this;
-    const { keyFile } = config;
-
-    console.log('init', keyFile);
-
-    fs.readFile(keyFile, (err, content) => {
-      console.log('key file:', err, content);
-      if(err) {
-        node.status({fill:"red",shape:"ring",text:"Key file not found"});
-      }
-      else {
-        try {
-          node.key = JSON.parse(content.toString());
-          afterInit();
-        }
-        catch(e) {
-          node.status({fill:"red",shape:"ring",text:"Key file invalid"});
-        }
-      }
-    });
-    function afterInit() {
-      node.status({fill:"green",shape:"dot",text:"connected"});
-			node.on('input', msg => {
-				if(msg.roomId) {
-					postMessage(msg.payload, node.key, msg.roomId);
+    const node = this;
+    const varPath = (config['varName'] || 'googlekeys').split('.');
+    node.status({fill:"grey",shape:"dot",text:"waiting for message"});
+    node.on('input', msg => {
+      const varValue = varPath.reduce((acc, val) => acc = acc[val], msg);
+      if(!!varValue) {
+				node.key = populateKey(varValue);
+				if(node.key) {
+					if(msg.roomId) {
+						node.status({fill:"green",shape:"dot",text:"sending"});
+						postMessage(msg.payload, node.key, msg.roomId)
+							.then(res => {
+								node.status({fill:"green",shape:"dot",text:"connected"});
+							})
+							.catch(e => {
+								console.error('Could not send message to hangouts chat', e);
+								node.error('Could not send message to hangouts chat', e);
+								node.status({fill:"red",shape:"ring",text:e});
+							})
+					}
+					else {
+						console.error('Input has to provide a roomId field');
+						node.error('Input has to provide a roomId field');
+						node.status({fill:"red",shape:"ring",text:"Input has to provide a roomId field"});
+					}
 				}
 				else {
-					console.error('Input has to provide a roomId field');
-					node.error('Input has to provide a roomId field');
-        node.status({fill:"red",shape:"ring",text:"Input has to provide a roomId field"});
+					node.status({fill:"red",shape:"ring",text:"Key file invalid"});
+					node.error('Key file is invalid');
 				}
-			});
+			}
+      else {
+        console.error(`Input has to provide a msg.${varPath.join('.')} field`);
+        node.error(`Input has to provide a msg.${varPath.join('.')} field`);
+        node.status({fill:"red",shape:"ring",text:`Input has to provide a msg.${varPath.join('.')} field`});
+      }
+    });
+    function populateKey(content) {
+      try {
+        return JSON.parse(content);
+      }
+      catch(e) {
+				return null;
+      }
+    }
+
+
+    function getJWT(gkeys) {
+      return new Promise((resolve, reject) => {
+        let jwtClient = new google.auth.JWT(
+          gkeys.client_email,
+          null,
+          gkeys.private_key, ['https://www.googleapis.com/auth/chat.bot']);
+
+        jwtClient.authorize((err, tokens) => {
+          if (err) {
+            console.log('Error create JWT hangoutchat');
+            reject(err);
+          } else {
+            resolve(tokens.access_token);
+          }
+        });
+      });
+    }
+
+    function postMessage(message, key, roomId) {
+      console.log('post message to space', roomId);
+      return new Promise((resolve, reject) => {
+        if(key && key !== '') {
+          getJWT(key).then((token) => {
+            console.log('token', token);
+            unirest.post('https://chat.googleapis.com/v1/' + roomId + '/messages')
+              .headers({
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + token
+              })
+              .send(JSON.stringify({
+                'text': message,
+              }))
+              .end((res) => {
+                console.log('sent', res.body);
+                resolve();
+              });
+          }).catch((err) => {
+            reject(err);
+          });
+        }
+        else {
+          reject('Error: missing key file');
+        }
+      });
     }
   }
-
   RED.nodes.registerType("hangouts-chat-out", HangoutsChatOutNode);
-
-};
-
-function getJWT(gkeys) {
-	return new Promise((resolve, reject) => {
-		let jwtClient = new google.auth.JWT(
-			gkeys.client_email,
-			null,
-			gkeys.private_key, ['https://www.googleapis.com/auth/chat.bot']);
-
-		jwtClient.authorize((err, tokens) => {
-			if (err) {
-				console.log('Error create JWT hangoutchat');
-				reject(err);
-			} else {
-				resolve(tokens.access_token);
-			}
-		});
-	});
-}
-
-function postMessage(message, key, roomId) {
-	console.log('post message to space', roomId);
-	return new Promise((resolve, reject) => {
-		getJWT(key).then((token) => {
-			console.log('token', token);
-			unirest.post('https://chat.googleapis.com/v1/' + roomId + '/messages')
-				.headers({
-					"Content-Type": "application/json",
-					"Authorization": "Bearer " + token
-				})
-				.send(JSON.stringify({
-					'text': message,
-				}))
-				.end((res) => {
-					console.log('sent', res.body);
-					resolve();
-				});
-		}).catch((err) => {
-			reject(err);
-		});
-	});
 }
 
 
